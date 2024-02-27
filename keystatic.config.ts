3d4b54b6fbda1d7e98b326760d14ad54fa45390f
Dan Weinstein <dweinste1@gmail.com>
2024-02-27 14:05:43 -0800

    configure keystatic to edit github rather than local pages

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,7 +2,11 @@ import { config, fields, collection } from '@keystatic/core';
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: 'github',
+   repo: {
+     owner: 'brassdan',
+     name: 'creec'
+   }   
   },
   collections: {
     posts: collection({
